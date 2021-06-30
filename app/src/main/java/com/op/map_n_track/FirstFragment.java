68d68d49eff79e06cb11fc4db29cdc19baa51e60
zaid kamil <xaidmetamorphos@gmail.com>
2021-06-30 19:19:13 +0530

    map and address selection

--- app/src/main/java/com/op/map_n_track/FirstFragment.java
+++ app/src/main/java/com/op/map_n_track/FirstFragment.java
@@ -1,37 +1,46 @@
 package com.op.map_n_track;
 
-import android.app.Activity;
+import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import com.google.android.material.snackbar.BaseTransientBottomBar;
-import com.google.android.material.snackbar.Snackbar;
 import com.mapbox.api.geocoding.v5.models.CarmenFeature;
+import com.mapbox.geojson.Point;
 import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.annotations.MarkerOptions;
 import com.mapbox.mapboxsdk.camera.CameraPosition;
 import com.mapbox.mapboxsdk.camera.CameraUpdateFactory;
 import com.mapbox.mapboxsdk.geometry.LatLng;
+import com.mapbox.mapboxsdk.location.LocationComponent;
+import com.mapbox.mapboxsdk.location.LocationComponentActivationOptions;
+import com.mapbox.mapboxsdk.location.modes.CameraMode;
+import com.mapbox.mapboxsdk.location.modes.RenderMode;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.Style;
-import com.mapbox.mapboxsdk.plugins.places.autocomplete.PlaceAutocomplete;
-import com.mapbox.mapboxsdk.plugins.places.autocomplete.model.PlaceOptions;
+import com.mapbox.mapboxsdk.maps.UiSettings;
 import com.mapbox.mapboxsdk.plugins.places.picker.PlacePicker;
 import com.mapbox.mapboxsdk.plugins.places.picker.model.PlacePickerOptions;
 import com.op.map_n_track.databinding.FragmentFirstBinding;
 
+import static android.app.Activity.RESULT_OK;
+
 public class FirstFragment extends Fragment {
 
-    private static final int REQUEST_CODE_AUTOCOMPLETE = 21;
-    private static final int PLACE_SELECTION_REQUEST_CODE = 56789;
+    private static final int REQUEST_CODE_START = 5678;
+    private static final int REQUEST_CODE_DEST = 5679;
     private FragmentFirstBinding binding;
     private MapboxMap mapbox;
+    private LatLng lucknow;
+    private Point startCoord;
+    private Point destCoord;
 
     @Override
     public View onCreateView(
@@ -45,53 +54,75 @@ public class FirstFragment extends Fragment {
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
+        lucknow = new LatLng(26.8467, 80.9462);
         binding.mapView.onCreate(savedInstanceState);
-        binding.mapView.getMapAsync(mapboxMap -> mapboxMap.setStyle(Style.MAPBOX_STREETS, style -> {
+        binding.mapView.getMapAsync(mapboxMap -> mapboxMap.setStyle(Style.SATELLITE_STREETS, style -> {
             mapbox = mapboxMap;
-            LatLng lucknow = new LatLng(26.8467, 80.9462);
+            UiSettings settings = mapbox.getUiSettings();
+            settings.setCompassEnabled(true);
+            enableLocationComponent(style);
             mapbox.animateCamera(CameraUpdateFactory.newLatLngZoom(lucknow, 16), 3000);
             activateUI();
         }));
     }
 
+    @SuppressLint("MissingPermission")
+    private void enableLocationComponent(Style b) {
+        LocationComponent component = mapbox.getLocationComponent();
+        LocationComponentActivationOptions activationOptions = LocationComponentActivationOptions.builder(getActivity(), b).build();
+        component.activateLocationComponent(activationOptions);
+        component.setLocationComponentEnabled(true);
+        component.setCameraMode(CameraMode.TRACKING_GPS_NORTH);
+        component.setRenderMode(RenderMode.COMPASS);
+
+    }
+
     private void activateUI() {
-        binding.btnSearch.setOnClickListener(view -> {
-            PlaceOptions placeOptions = PlaceOptions.builder().backgroundColor(Color.WHITE).build();
-            Intent intent = new PlaceAutocomplete.IntentBuilder()
-                    .accessToken(getString(R.string.mapbox_access_token))
-                    .placeOptions(placeOptions)
-                    .build(getActivity());
-            startActivityForResult(intent, REQUEST_CODE_AUTOCOMPLETE);
+        CameraPosition camera = new CameraPosition.Builder().target(lucknow).zoom(16).build();
+        PlacePickerOptions options = PlacePickerOptions.builder()
+                .statingCameraPosition(camera)
+                .includeDeviceLocationButton(true)
+                .includeReverseGeocode(true).build();
+        Intent intent = new PlacePicker.IntentBuilder()
+                .accessToken(getString(R.string.mapbox_access_token))
+                .placeOptions(options)
+                .build(getActivity());
+        binding.textStart.setOnClickListener(view -> {
+            startActivityForResult(intent, REQUEST_CODE_START);
         });
-        binding.btnSearch.setOnLongClickListener(view -> {
-            Intent intent = new PlacePicker.IntentBuilder()
-                    .accessToken(Mapbox.getAccessToken())
-                    .placeOptions(
-                            PlacePickerOptions.builder()
-                                    .statingCameraPosition(
-                                            new CameraPosition.Builder()
-                                                    .target(new LatLng(26.8467, 80.9462))
-                                                    .zoom(16)
-                                                    .build())
-                                    .build())
-                    .build(getActivity());
-            startActivityForResult(intent, PLACE_SELECTION_REQUEST_CODE);
-            return true;
+        binding.textDest.setOnClickListener(view -> {
+            startActivityForResult(intent, REQUEST_CODE_DEST);
         });
     }
 
     @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+    public void onActivityResult(int requestCode, int resultCode, @Nullable @org.jetbrains.annotations.Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode == Activity.RESULT_OK && requestCode == REQUEST_CODE_AUTOCOMPLETE) {
-            CarmenFeature feature = PlaceAutocomplete.getPlace(data);
-            Snackbar.make(binding.getRoot(), feature.text(), BaseTransientBottomBar.LENGTH_INDEFINITE).show();
+        if (resultCode == RESULT_OK) {
+            switch (requestCode) {
+                case REQUEST_CODE_START:
+                    CarmenFeature place = PlacePicker.getPlace(data);
+                    if (place != null) {
+                        binding.textStart.setText(place.placeName());
+                        startCoord = place.center();
+                        mapbox.addMarker(new MarkerOptions().position(new LatLng(startCoord.latitude(), startCoord.longitude())));
+                    } else {
+                        binding.textStart.setText("No address found");
+                    }
+                    break;
+                case REQUEST_CODE_DEST:
+                    CarmenFeature place2 = PlacePicker.getPlace(data);
+                    if (place2 != null) {
+                        binding.textDest.setText(place2.placeName());
+                        destCoord = place2.center();
+                        mapbox.addMarker(new MarkerOptions().position(new LatLng(destCoord.latitude(), destCoord.longitude())));
+                    } else {
+                        binding.textDest.setText("No address found");
+                    }
 
-        }
-        if (resultCode == Activity.RESULT_OK && requestCode == PLACE_SELECTION_REQUEST_CODE) {
-            CarmenFeature carmenFeature = PlacePicker.getPlace(data);
-            Snackbar.make(binding.getRoot(), carmenFeature.text(), BaseTransientBottomBar.LENGTH_INDEFINITE).show();
+            }
+        } else {
+            Toast.makeText(getActivity(), "you cancelled the action", Toast.LENGTH_SHORT).show();
         }
     }
 
